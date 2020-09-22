eeef304ca9d8792f6d687240f0e7fb501c96e0e9
akshat1032 <akshat.30oct@gmail.com>
2020-09-22 15:49:01 +0530

    start

---
+++ EWP/src/demoprob/EmployeeWageComputation.java
@@ -0,0 +1,9 @@
+package demoprob;
+
+public class EmployeeWageComputation {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program");
+	}
+
+}
