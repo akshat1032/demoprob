d1b754357b68bd38b85b4b35fb7d099a45a8f397
akshat1032 <akshat.30oct@gmail.com>
2020-09-22 16:24:49 +0530

    Daily Wage Computed

--- EWP/src/demoprob/EmployeeWageComputation.java
+++ EWP/src/demoprob/EmployeeWageComputation.java
@@ -2,7 +2,14 @@ package demoprob;
 
 public class EmployeeWageComputation {
 	
+	//constant
+	public static final int WAGE_PER_HOUR = 20;
+	public static final int HOUR_PER_DAY = 8;
+	
 	public static void main(String[] args) {
+		
+		//variable
+		double dailyWage = 0;
 		
 		//storing attendance
 		int empAttendance = (int)(Math.floor(Math.random() * 10) % 2);
@@ -10,10 +17,13 @@ public class EmployeeWageComputation {
 		//checking
 		if (empAttendance == 1) {
 			System.out.println("Employee present");
+			dailyWage = WAGE_PER_HOUR * HOUR_PER_DAY;
 		}
 		else {
 			System.out.println("Employee absent");
 		}
+		
+		System.out.println("Daily Wage : "+dailyWage);
 	}
 
 }
