5c418b211dc19c70eb56254e651f77f7f41dcd12
akshat1032 <akshat.30oct@gmail.com>
2020-09-22 16:01:08 +0530

    use case 1

--- EWP/src/demoprob/EmployeeWageComputation.java
+++ EWP/src/demoprob/EmployeeWageComputation.java
@@ -3,7 +3,17 @@ package demoprob;
 public class EmployeeWageComputation {
 	
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program");
+		
+		//storing attendance
+		int empAttendance = (int)(Math.floor(Math.random() * 10) % 2);
+		
+		//checking
+		if (empAttendance == 1) {
+			System.out.println("Employee present");
+		}
+		else {
+			System.out.println("Employee absent");
+		}
 	}
 
 }
